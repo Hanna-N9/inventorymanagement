5157f477bf3e24176d2a5b63a82515c8885c0af0
Hanna Negash <hnegash.n@gmail.com>
2023-12-22 14:27:35 -0800

    Add more code to file to enter ipdb session

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-# lib/debug.py
 
 from models.__init__ import CONN, CURSOR
+from models.category import Category
+
 import ipdb
 
+Category.drop_table()
+Category.create_table()
 
 ipdb.set_trace()
