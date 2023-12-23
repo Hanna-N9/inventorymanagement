80599e39996d767276b9da7ce9f8634b0f1e0f73
Hanna Negash <hnegash.n@gmail.com>
2023-12-23 10:58:05 -0800

    Remove second field/argument and print

--- lib/debug.py
+++ lib/debug.py
@@ -10,44 +10,15 @@ def reset_database():
     Category.create_table()
     
     # Create seed data
-    tv = Category.create("Electronics", "TV")
-    print(tv) 
     
-    table = Category.create("Furniture", "Tables")
-    print(table)  
+    electronic = Category.create("Electronics")
+    furniture = Category.create("Furniture")
+    food = Category.create("Food")
+    clothing = Category.create("Clothing")
+    sport = Category.create("Sport Equipment")
+    book = Category.create("Books")
+    kitchen = Category.create("Kitchen")
+    home = Category.create("Home Appliances")
     
-    fruit = Category.create("Food", "Fruits")
-    print(fruit) 
-    
-    f = Category.create("Clothing", "Women")
-    print(f)  
-    
-    football = Category.create("Sports", "Football")
-    print(football)
-    
-    kitchenware = Category.create("Home Decor", "Kitchenware")
-    print(kitchenware)
-    
-    novels = Category.create("Books", "Novels")
-    print(novels)
-    
-    textbooks = Category.create("Books", "Textbooks")
-    print(textbooks)
-    
-    shoe = Category.create("Sport Equipment", "Running Shoe")
-    print(shoe)
-    
-    system = Category.create("Software", "Operating Systems")
-    print(system)
-    
-    cookware = Category.create("Kitchen", "Cookware")
-    print(cookware)
-    
-    vegetable = Category.create("Food", "Vegetable")
-    print(vegetable)
-    
-    vegetable = Category.create("Home Appliances", "Refrigerators")
-    print(vegetable)
-
 reset_database()
 ipdb.set_trace()
